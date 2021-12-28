3f41ed24662827f72e73e2fdc22bd0b93443ee34
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-12-29 06:57:30 +0700

    update const

--- const.js
+++ const.js
@@ -9,3 +9,23 @@
 const PI = 3.1415;
 PI = 3.14;      // This will give an error
 PI = PI + 1;   // This will also give an error
+
+//constanst array
+// You can create a constant array:
+const teman = ["abi", "xian", "tom"];
+
+// You can change an element:
+teman[0] = "rexy";
+
+// You can add an element:
+teman.push("yogi");
+
+// Constant Objects
+// You can create a const object:
+const hp = {type:"android", model:"vivo", color:"black"};
+
+// You can change a property:
+hp.color = "red";
+
+// You can add a property:
+hp.owner = "abi";
