1c707337820c65598ef4ccb13a64c7345ac5dd50
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-12-29 06:54:09 +0700

    add const

---
+++ const.js
@@ -0,0 +1,11 @@
+//const
+
+// Variables defined with const cannot be Redeclared.
+
+// Variables defined with const cannot be Reassigned.
+
+// Variables defined with const have Block Scope.
+
+const PI = 3.1415;
+PI = 3.14;      // This will give an error
+PI = PI + 1;   // This will also give an error
