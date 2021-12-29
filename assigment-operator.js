6776cde475e41c0043269b275b2260af4e432e4a
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-12-29 07:33:31 +0700

    assigment-operator.js

---
+++ assigment-operator.js
@@ -0,0 +1,18 @@
+let a = 10
+a += 1;
+let b = 10
+b -= 1;
+let c = 10
+c *= 2;
+let d = 10
+d **= 2;
+let e = 10
+e /= 2;
+let f = 10
+f %= 2;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
