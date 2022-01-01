540308fefeec8f018faf9f25e84c1eb9f40916ca
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-01-01 12:21:13 +0700

    update

---
+++ Basic/aritmethic-operator.js
@@ -0,0 +1,13 @@
+let a = 1 +1;
+let b = 2 - 1;
+let c = 2 * 2;
+let d = 2 ** 2;
+let e = 2 / 2;
+let f = 2 % 2;
+ 
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
---
+++ Basic/escape-sequence.js
@@ -0,0 +1,16 @@
+// Escape Sequences in Strings
+
+/*
+
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\r	carriage return
+\t	tab
+\b	word boundary
+\f	form feed
+
+*/
+const str = "Abi said, \"dani is learning JavaScript\".";
+console.log(str);
