cb66ba4b8334a06667736218b699aa7892c4c078
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-02-11 20:55:50 +0700

    add object

---
+++ Basic/object.js
@@ -0,0 +1,15 @@
+const person = {
+    name: 'abi',
+    age: 20,
+    location: {
+        city: 'malang',
+        provience: 'jawa timur',
+    },
+    hobby: ['coding', 'reading', 'listening music'],
+}
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.location.city);
+console.log(person.hobby[1]);
+
