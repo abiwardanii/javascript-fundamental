b125ccbf87e1fa5d45bfcff10f793d81e9147e77
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-04-20 19:54:40 +0700

    add daet

---
+++ standard-library/date/date.js
@@ -0,0 +1,5 @@
+const date1 = new Date();
+console.log(date1);
+
+const date2 = new Date(2022, 4, 18);
+console.log(date2);
---
+++ standard-library/date/unix-timestamp.js
@@ -0,0 +1,5 @@
+const timestamp = Date.now();
+console.log(timestamp);
+
+const timestamp2 = new Date(2022, 4, 18);
+console.log(timestamp2.getTime());
